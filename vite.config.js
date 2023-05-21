ca87c399f9e76cd4e1c1a24a0a4f883650d0199b
wayand bahramzy <lawangjan@hotmail.com>
2023-05-21 23:57:54 +0200

    using the theme Mazer scss modular

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url';
+const path = require('path')
 
 export default defineConfig({
     plugins: [vue()],
@@ -8,11 +9,15 @@ export default defineConfig({
         extensions: ['.js', '.vue'],
         alias: {
             "@": fileURLToPath(new URL('./src', import.meta.url)),
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons'),
+            '~perfect-scrollbar': path.resolve(__dirname, 'node_modules/perfect-scrollbar'),
         }
     },
     server: {
         port: 8080,
-        host: 'invoicer.local'
+        host: 'invoicer.local',
+        hot: true
     },
     // build: {
     //     outDir: 'dist'
