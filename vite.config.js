ef6c326e416b85896b1c259fa651c849b9b2535f
Wayand Bahramzy <lawangjan@hotmail.com>
2025-11-22 00:03:44 +0100

    fix vite.config path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url';
-const path = require('path')
+import path from 'path'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 export default defineConfig({
